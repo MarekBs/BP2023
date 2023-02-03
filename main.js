82deec5cde7ebee086620f14495d4debf77efd0f
Marek Belis <marek.belis810@gmail.com>
2023-02-03 13:23:51 +0100

    fixed header background

--- main.js
+++ main.js
@@ -2,14 +2,19 @@ let swch = document.getElementById("flexSwitchCheckDefault");
 
 function changeMode(){
     if(swch.checked){
-        document.body.style.backgroundImage = "url('images/nightBG.png')";
+        console.log(document.getElementsByTagName("header"))
+        document.querySelector("header").style.backgroundImage = "url('images/nightBG.png')";
         document.body.style.backgroundColor = "grey";
         document.getElementById("header-container").style.backgroundColor = "rgba(255,255,255,0.6)"
 ;
     }
     else {
-        document.body.style.backgroundImage = "url('images/headBG.png')";
+        document.querySelector("header").style.backgroundImage = "url('images/headBG.png')";
         document.body.style.backgroundColor = "#f3f4f6";
         document.getElementById("header-container").style.backgroundColor = "rgba(255, 69, 0, 0.1)"
     }
+    
 }
+
+
+
