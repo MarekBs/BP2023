479d9f10fe92ea8fdb82426a3aa874a7ce5efa67
Marek Belis <marek.belis810@gmail.com>
2023-02-04 20:42:34 +0100

    added few folders for better maintenance

--- landingPage/main.js
+++ landingPage/main.js
@@ -3,13 +3,13 @@ let swch = document.getElementById("flexSwitchCheckDefault");
 function changeMode(){
     if(swch.checked){
         console.log(document.getElementsByTagName("header"))
-        document.querySelector("header").style.backgroundImage = "url('images/nightBG.png')";
+        document.querySelector("header").style.backgroundImage = "url('../images/nightBG.png')";
         document.body.style.backgroundColor = "grey";
         document.getElementById("header-container").style.backgroundColor = "rgba(255,255,255,0.6)"
 ;
     }
     else {
-        document.querySelector("header").style.backgroundImage = "url('images/headBG.png')";
+        document.querySelector("header").style.backgroundImage = "url('../images/headBG.png')";
         document.body.style.backgroundColor = "#f3f4f6";
         document.getElementById("header-container").style.backgroundColor = "rgba(255, 69, 0, 0.1)"
     }
