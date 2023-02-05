55b076bbdce74f849c850782c44947f23f54c801
Marek Belis <marek.belis810@gmail.com>
2023-02-05 20:42:17 +0100

    added new svj bg

--- landingPage/main.js
+++ landingPage/main.js
@@ -1,17 +1,21 @@
 let swch = document.getElementById("flexSwitchCheckDefault");
+let header = document.querySelector("header");
+let headerContainer = document.getElementById("header-container");
+let nav = document.querySelector(".navbar");
 
 function changeMode(){
     if(swch.checked){
-        console.log(document.getElementsByTagName("header"))
-        document.querySelector("header").style.backgroundImage = "url('../images/nightBG.png')";
+        header.style.backgroundImage = "url('../images/nightBG1.svg')";
         document.body.style.backgroundColor = "grey";
-        document.getElementById("header-container").style.backgroundColor = "rgba(255,255,255,0.6)"
+        headerContainer.style.backgroundColor = "rgba(255,255,255,0.6)";
+        nav.style.backgroundColor = "#101722";
 ;
     }
     else {
-        document.querySelector("header").style.backgroundImage = "url('../images/headBG.png')";
+        header.style.backgroundImage = "url('../images/mainBG.svg')";
         document.body.style.backgroundColor = "#f3f4f6";
-        document.getElementById("header-container").style.backgroundColor = "rgba(255, 69, 0, 0.1)"
+        headerContainer.style.backgroundColor = "rgba(255, 69, 0, 0.1)";
+        nav.style.backgroundColor = "#1c2c34";
     }
     
 }
