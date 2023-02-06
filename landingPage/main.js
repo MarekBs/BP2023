2fc87acf138293421ea0c29da0b8d3670e631925
Marek Belis <marek.belis810@gmail.com>
2023-02-06 19:16:09 +0100

    added function changeColors() in main.js

--- landingPage/main.js
+++ landingPage/main.js
@@ -8,34 +8,29 @@ let line = document.getElementById("line")
 
 
 function changeMode(){
+    
     if(swch.checked){
-        header.style.backgroundImage = "url('../images/nightBG1.svg')";
-        document.body.style.backgroundColor = "#172130";
-        headerContainer.style.backgroundColor = "rgba(0,0,0,0.6)";
-        headerContainer.style.color = "white";
-        nav.style.backgroundColor = "#101722";
-        intro.style.color = "white";
-        for (let i= 0; i< nadpis.length ; i++){
-            nadpis[i].style.color = "white"
-        }
-        line.style.color = "white"
+        changeColors("url('../images/nightBG1.svg')","#172130","rgba(0,0,0,0.6)","white","#101722","white","white","white");
         
 ;
     }
     else {
-        header.style.backgroundImage = "url('../images/mainBG.svg')";
-        document.body.style.backgroundColor = "#f3f4f6";
-        headerContainer.style.backgroundColor = "rgba(255, 69, 0, 0.2)";
-        headerContainer.style.color = "black";
-        nav.style.backgroundColor = "#1c2c34";
-        intro.style.color = "grey";
-        for (let i= 0; i< nadpis.length ; i++){
-            nadpis[i].style.color = "black"
-        }
-        line.style.color = "black"
+        changeColors("url('../images/mainBG.svg')","#f3f4f6","rgba(255, 69, 0, 0.2)","black","#1c2c34","grey","black","black");
+        
     }
     
 }
 
 
-
+function changeColors(image,bodyColor,headerContainerColor,headerTextColor,navBg,introColor,nadpisColor,lineColor){
+    header.style.backgroundImage = image;
+        document.body.style.backgroundColor = bodyColor;
+        headerContainer.style.backgroundColor = headerContainerColor;
+        headerContainer.style.color = headerTextColor;
+        nav.style.backgroundColor = navBg;
+        intro.style.color = introColor;
+        for (let i= 0; i< nadpis.length ; i++){
+            nadpis[i].style.color = nadpisColor
+        }
+        line.style.color = lineColor;
+}
