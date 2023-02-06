ead4a0adf3e723279b02d55a4fb9e4fe819da452
Marek Belis <marek.belis810@gmail.com>
2023-02-06 17:36:38 +0100

    added info text

--- landingPage/main.js
+++ landingPage/main.js
@@ -2,14 +2,16 @@ let swch = document.getElementById("flexSwitchCheckDefault");
 let header = document.querySelector("header");
 let headerContainer = document.getElementById("header-container");
 let nav = document.querySelector(".navbar");
+let intro = document.getElementById("intro");
 
 function changeMode(){
     if(swch.checked){
         header.style.backgroundImage = "url('../images/nightBG1.svg')";
-        document.body.style.backgroundColor = "grey";
+        document.body.style.backgroundColor = "#172130";
         headerContainer.style.backgroundColor = "rgba(0,0,0,0.6)";
         headerContainer.style.color = "white";
         nav.style.backgroundColor = "#101722";
+        intro.style.color = "white";
 ;
     }
     else {
@@ -18,6 +20,7 @@ function changeMode(){
         headerContainer.style.backgroundColor = "rgba(255, 69, 0, 0.2)";
         headerContainer.style.color = "black";
         nav.style.backgroundColor = "#1c2c34";
+        intro.style.color = "grey";
     }
     
 }
