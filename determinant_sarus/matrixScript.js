6a25e913ff832bf6c93ea7e3ba7822b19557ae0e
Marek Belis <marek.belis810@gmail.com>
2023-02-28 21:21:58 +0100

    fixed width of main, no longer overlaping

--- determinant_sarus/matrixScript.js
+++ determinant_sarus/matrixScript.js
@@ -117,3 +117,14 @@ btn1.addEventListener("click",function(){
 
 
 
+// get references to the sidebar and main elements
+const sidebar = document.querySelector('.sidebar');
+const main = document.querySelector('main');
+
+// update the margin of the main element on window resize
+function updateMainMargin() {
+  const sidebarWidth = sidebar.offsetWidth;
+  main.style.marginLeft = `${sidebarWidth}px`;
+}
+
+window.addEventListener('resize', updateMainMargin);
