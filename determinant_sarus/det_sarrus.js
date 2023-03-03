7d9d265890b2ed3ab65686f136fde7893065fce7
Marek Belis <marek.belis810@gmail.com>
2023-03-03 13:04:45 +0100

    fixed bootstrap errors, adjusted js files

--- determinant_sarus/det_sarrus.js
+++ determinant_sarus/det_sarrus.js
@@ -21,105 +21,96 @@ function loadMartix() {
 }
 
 function calcDet(matrix, index) {
-    outputDiv.innerHTML= " = ";
-    console.log(index);
-    let det = [];
-    let nasobok = [];
+  outputDiv.innerHTML = " = ";
+  console.log(index);
+  let det = [];
+  let nasobok = [];
 
-    for (let i = 0; i < matrix[0].length; i++) {
-        let a = matrix[i][index];
+  for (let i = 0; i < matrix[0].length; i++) {
+    let a = matrix[i][index];
 
-        let aPoz = a * (-1) ** (index+1 + (i + 1));
+    let aPoz = a * (-1) ** (index + 1 + (i + 1));
 
-        let matr = [
-            [4],
-            [4],
-            [4],
-            [4]
-        ];
-        
-        for (let f = 0; f < 4; f++) {
-            for (let g = 0; g < 4; g++) {
-                matr[f][g] = matrix[f][g];
-            }
-        }
-        
-        matr.splice(i, 1);
+    let matr = [[4], [4], [4], [4]];
 
-        for (let x = 0; x < matrix[0].length - 1; x++) {
-            matr[x].splice(index, 1);
-        }
-
-        console.log(aPoz + "*", matr, "+");
-
-        det[i] = math.det(matr);
-        nasobok[i] = aPoz;
-
-        
-        let step;
-        if (nasobok[i] < 0) {
-            step = "(" + nasobok[i] + ") *     ";
-        } else {
-            step = nasobok[i] + " *     ";
-        }
-
-
-        // create table
-        step += "<table style='display: inline-block; border-right: 1px solid grey; border-left: 1px solid grey; margin: 0 6px;'>";
-        for (let j = 0; j < matr.length; j++) {
-            step += "<tr>";
-            for (let k = 0; k < matr[j].length; k++) {
-                step += "<td>" + matr[j][k] + "</td>";
-            }
-            step += "</tr>";
-        }
-        step += "</table>";
-        cnt++;
-        if (cnt < matrix[0].length) {
-            step += "  +  ";
-        }
-        if(cnt== matrix[0].length){
-           step += " = " 
-        }
-
-        
-        outputDiv.innerHTML += step;
-        
+    for (let f = 0; f < 4; f++) {
+      for (let g = 0; g < 4; g++) {
+        matr[f][g] = matrix[f][g];
+      }
     }
-    
-    let vysledok = 0;
-    
+
+    matr.splice(i, 1);
+
+    for (let x = 0; x < matrix[0].length - 1; x++) {
+      matr[x].splice(index, 1);
+    }
+
+    console.log(aPoz + "*", matr, "+");
+
+    det[i] = math.det(matr);
+    nasobok[i] = aPoz;
+
     let step;
-    for (let i = 0; i < 4; i++) {
-        vysledok += nasobok[i] * det[i];
-        let nasob;
-        let deter;
-
-        if(nasobok[i]<0){
-            nasob = "("+Math.round(nasobok[i])+")";
-        }else {
-            nasob = Math.round(nasobok[i]);
-        }
-
-        if(det[i]<0){
-            deter = "("+Math.round(det[i])+")";
-        }else {
-            deter = det[i];
-        }
-
-        if(i<3){
-          step = nasob  + " * " + deter  + " + "; 
-        }else {
-            step = nasob  + " * " +deter + " = " + vysledok; 
-        }
-        
-        outputDiv.innerHTML += step;
-        console.log(nasobok[i] + "*" + det[i]);
-        
+    if (nasobok[i] < 0) {
+      step = "(" + nasobok[i] + ") *     ";
+    } else {
+      step = nasobok[i] + " *     ";
     }
 
-    console.log("=" + vysledok);
-    cnt=0;
+    // create table
+    step +=
+      "<table style='display: inline-block; border-right: 1px solid grey; border-left: 1px solid grey; margin: 0 6px;'>";
+    for (let j = 0; j < matr.length; j++) {
+      step += "<tr>";
+      for (let k = 0; k < matr[j].length; k++) {
+        step += "<td>" + matr[j][k] + "</td>";
+      }
+      step += "</tr>";
+    }
+    step += "</table>";
+    cnt++;
+    if (cnt < matrix[0].length) {
+      step += "  +  ";
+    }
+    if (cnt == matrix[0].length) {
+      step += " = ";
+    }
+
+    outputDiv.innerHTML += step;
+  }
+
+  let vysledok = 0;
+
+  let step;
+  for (let i = 0; i < 4; i++) {
+    vysledok += nasobok[i] * det[i];
+    let nasob;
+    let deter;
+
+    if (nasobok[i] < 0) {
+      nasob = "(" + Math.round(nasobok[i]) + ")";
+    } else {
+      nasob = Math.round(nasobok[i]);
+    }
+
+    if (det[i] < 0) {
+      deter = "(" + Math.round(det[i]) + ")";
+    } else {
+      deter = det[i];
+    }
+
+    if (i < 3) {
+      step = nasob + " * " + deter + " + ";
+    } else {
+      step = nasob + " * " + deter + " = " + vysledok;
+    }
+
+    outputDiv.innerHTML += step;
+    console.log(nasobok[i] + "*" + det[i]);
+  }
+
+  console.log("=" + vysledok);
+  cnt = 0;
 }
 
-calcDet(matrix,1)
+/* calcDet(matrix,1) */
--- determinant_sarus/matrixScript.js
+++ determinant_sarus/matrixScript.js
@@ -1,131 +1,131 @@
-const matrixSizeInput = document.getElementById('matrix-size');
-      const matrixInputTable = document.getElementById('matrix-input').querySelector('tbody');
+const matrixSizeInput = document.getElementById("matrix-size");
+const matrixInputTable = document
+  .getElementById("matrix-input")
+  .querySelector("tbody");
 
-      function createMatrix() {
-        const matrixSize = parseInt(matrixSizeInput.value);
-        matrixInputTable.innerHTML = '';
-    
-        for (let i = 0; i < matrixSize; i++) {
-          const row = document.createElement('tr');
-    
-          for (let j = 0; j < matrixSize; j++) {
-            const cell = document.createElement('td');
-            const input = document.createElement('input');
-    
-            input.type = 'number';
-            input.name = `matrix[${i}][${j}]`;
-            input.placeholder = "0";
-            input.style.width = '1.5rem';
-            input.style.textAlign = "center";
-            input.style.border = "none";
-            input.classList.add("inputNum");
-    
-            // add event listeners for arrow keys
-            input.addEventListener('keydown', (event) => {
-              if (event.key === 'ArrowRight') {
-                const nextCell = cell.nextElementSibling;
-                if (nextCell) {
-                  nextCell.querySelector('input').focus();
-                }
-              } else if (event.key === 'ArrowLeft') {
-                const prevCell = cell.previousElementSibling;
-                if (prevCell) {
-                  prevCell.querySelector('input').focus();
-                }
-              } else if (event.key === 'ArrowDown') {
-                event.preventDefault();
-                const nextRow = row.nextElementSibling;
-                if (nextRow) {
-                  nextRow.children[j].querySelector('input').focus();
-                }
-              } else if (event.key === 'ArrowUp') {
-                event.preventDefault();
-                const prevRow = row.previousElementSibling;
-                if (prevRow) {
-                  prevRow.children[j].querySelector('input').focus();
-                }
-              }
-            });
-    
-            cell.appendChild(input);
-            row.appendChild(cell);
+function createMatrix() {
+  const matrixSize = parseInt(matrixSizeInput.value);
+  matrixInputTable.innerHTML = "";
+
+  for (let i = 0; i < matrixSize; i++) {
+    const row = document.createElement("tr");
+
+    for (let j = 0; j < matrixSize; j++) {
+      const cell = document.createElement("td");
+      const input = document.createElement("input");
+
+      input.type = "number";
+      input.name = `matrix[${i}][${j}]`;
+      input.placeholder = "0";
+      input.style.width = "1.5rem";
+      input.style.textAlign = "center";
+      input.style.border = "none";
+      input.classList.add("inputNum");
+
+      // add event listeners for arrow keys
+      input.addEventListener("keydown", (event) => {
+        if (event.key === "ArrowRight") {
+          const nextCell = cell.nextElementSibling;
+          if (nextCell) {
+            nextCell.querySelector("input").focus();
           }
-    
-          matrixInputTable.appendChild(row);
+        } else if (event.key === "ArrowLeft") {
+          const prevCell = cell.previousElementSibling;
+          if (prevCell) {
+            prevCell.querySelector("input").focus();
+          }
+        } else if (event.key === "ArrowDown") {
+          event.preventDefault();
+          const nextRow = row.nextElementSibling;
+          if (nextRow) {
+            nextRow.children[j].querySelector("input").focus();
+          }
+        } else if (event.key === "ArrowUp") {
+          event.preventDefault();
+          const prevRow = row.previousElementSibling;
+          if (prevRow) {
+            prevRow.children[j].querySelector("input").focus();
+          }
         }
-      }
-    
-      // create the initial matrix
-      createMatrix();
-    
-      // update the matrix when the user changes the size input
-      matrixSizeInput.addEventListener('change', createMatrix);
+      });
 
-
-
-
-      function changeMatrix() {
-        var size = document.getElementById("size").value;
-        var table = document.getElementById("matrix");
-        table.innerHTML = "";
-        for (var i = 1; i <= size; i++) {
-            var row = document.createElement("tr");
-            for (var j = 1; j <= size; j++) {
-                var cell = document.createElement("td");
-                var input = document.createElement("input");
-                input.type = "number";
-                input.placeholder = "0";
-                input.style.width = '1.5rem';
-                input.style.textAlign = "center";
-                input.style.border = "none";
-                input.classList.add("inputNum");
-                cell.appendChild(input);
-                row.appendChild(cell);
-            }
-            table.appendChild(row);
-        }
+      cell.appendChild(input);
+      row.appendChild(cell);
     }
 
-    function getMatrix(size,mtx) {
-        var size = document.getElementById(size).value;
-        var matrix = [];
-        for (var i = 1; i <= size; i++) {
-            var row = [];
-            for (var j = 1; j <= size; j++) {
-                var input = document.getElementById(mtx).rows[i-1].cells[j-1].getElementsByTagName("input")[0];
-                row.push(parseFloat(input.value));
-            }
-            matrix.push(row);
-        }
-        return matrix;
+    matrixInputTable.appendChild(row);
+  }
+}
+
+// create the initial matrix
+createMatrix();
+
+// update the matrix when the user changes the size input
+matrixSizeInput.addEventListener("change", createMatrix);
+
+function changeMatrix() {
+  var size = document.getElementById("size").value;
+  var table = document.getElementById("matrix");
+  table.innerHTML = "";
+  for (var i = 1; i <= size; i++) {
+    var row = document.createElement("tr");
+    for (var j = 1; j <= size; j++) {
+      var cell = document.createElement("td");
+      var input = document.createElement("input");
+      input.type = "number";
+      input.placeholder = "0";
+      input.style.width = "1.5rem";
+      input.style.textAlign = "center";
+      input.style.border = "none";
+      input.classList.add("inputNum");
+      cell.appendChild(input);
+      row.appendChild(cell);
     }
+    table.appendChild(row);
+  }
+}
+
+function getMatrix(size, mtx) {
+  var size = document.getElementById(size).value;
+  var matrix = [];
+  for (var i = 1; i <= size; i++) {
+    var row = [];
+    for (var j = 1; j <= size; j++) {
+      var input = document
+        .getElementById(mtx)
+        .rows[i - 1].cells[j - 1].getElementsByTagName("input")[0];
+      row.push(parseFloat(input.value));
+    }
+    matrix.push(row);
+  }
+  return matrix;
+}
 
 let btn = document.getElementById("calcBTN1");
 
-btn.addEventListener("click",function(){
-    let mat =getMatrix("size","matrix");
-    let index = document.getElementById("index").value;
-    calcDet(mat,index-1);
+btn.addEventListener("click", function () {
+  let mat = getMatrix("size", "matrix");
+  let index = document.getElementById("index").value;
+  calcDet(mat, index - 1);
 });
 
 let btn1 = document.getElementById("calcBTN");
 
-btn1.addEventListener("click",function(){
-    let matrx =getMatrix("matrix-size","matrix-input");
-    let result = math.det(matrx);
-    document.getElementById("mxVysledok").textContent = "="+" "+math.det(result)
+btn1.addEventListener("click", function () {
+  let matrx = getMatrix("matrix-size", "matrix-input");
+  let result = math.det(matrx);
+  document.getElementById("mxVysledok").textContent =
+    "=" + " " + math.det(result);
 });
 
-
-
 // get references to the sidebar and main elements
-const sidebar = document.querySelector('.sidebar');
-const main = document.querySelector('main');
+const sidebar = document.querySelector(".sidebar");
+const main = document.querySelector("main");
 
 // update the margin of the main element on window resize
 function updateMainMargin() {
   const sidebarWidth = sidebar.offsetWidth;
   main.style.marginLeft = `${sidebarWidth}px`;
 }
-
-window.addEventListener('resize', updateMainMargin);
+updateMainMargin();
+window.addEventListener("resize", updateMainMargin);
