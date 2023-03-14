2b814a93b4c9d478b99b1ba44bab5b557622af30
Marek Belis <marek.belis810@gmail.com>
2023-03-14 22:08:34 +0100

    fixed size of main

--- linearnaAlgebra/témy/detStvMatice/matrixScript.js
+++ linearnaAlgebra/témy/detStvMatice/matrixScript.js
@@ -127,6 +127,8 @@ const main = document.querySelector("main");
 function updateMainMargin() {
   const sidebarWidth = sidebar.offsetWidth;
   main.style.marginLeft = `${sidebarWidth}px`;
+  const availableWidth = document.body.clientWidth - sidebarWidth;
+  main.style.width = `${availableWidth}px`;
 }
 updateMainMargin();
 window.addEventListener("resize", updateMainMargin);
