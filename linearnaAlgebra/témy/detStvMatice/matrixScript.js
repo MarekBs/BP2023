ced4784ba33a76d90d17219bf8b21784f8dc6b02
Marek Belis <marek.belis810@gmail.com>
2023-04-18 12:01:51 +0200

    added determinant validation with outputs

--- linearnaAlgebra/témy/detStvMatice/matrixScript.js
+++ linearnaAlgebra/témy/detStvMatice/matrixScript.js
@@ -9,23 +9,35 @@ let btn = document.getElementById("calcBTN1");
 
 btn.addEventListener("click", function () {
   let mat = getMatrix("matrixSize", "matrix");
-  let index = document.getElementById("index").value;
+  if(mat == 1){
+    $( "#mxVysledok1")[0].innerHTML=` = <span style="color: red;"> vyplň všetky vstupy !</span>`;
+  }else {
+    let index = document.getElementById("index").value;
   calcDet(mat, index - 1);
+  }
+  
 });
 
 let btn1 = document.getElementById("calcBTN");
 
 btn1.addEventListener("click", function () {
   let matrx = getMatrix("matrix-size", "matrix-input");
-  let result = math.det(matrx);
+  if (matrx ==1){
+    $( "#mxVysledok")[0].innerHTML=` = <span style="color: red;"> vyplň všetky vstupy !</span>`;
+  }else {
+    let result = math.det(matrx);
   document.getElementById("mxVysledok").textContent =
     "=" + " " + math.det(Math.round(result));
+  }
+  
 });
 
 $( "#clearBTN" ).click(function() {
   $( "#mxVysledok1")[0].innerHTML=" = ";
+  changeMatrix();
 });
 
 $( "#clearBTN2" ).click(function() {
   $( "#mxVysledok")[0].innerHTML=" = ";
+  createMatrix();
 });
